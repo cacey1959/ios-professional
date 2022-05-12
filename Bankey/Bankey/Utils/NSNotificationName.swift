440f026109476834526f7cdd1bd167ee38fdcbc6
cacey1959 <81583051+cacey1959@users.noreply.github.com>
2022-05-11 23:39:49 -0400

    Add files via upload

---
+++ Bankey/Bankey/Utils/NSNotificationName.swift
@@ -0,0 +1,12 @@
+//
+//  NSNotificationName.swift
+//  Bankey
+//
+//  Created by Karen  on 5/10/22.
+//
+
+import Foundation
+
+extension Notification.Name {
+     static let logout = Notification.Name("Logout")
+}
